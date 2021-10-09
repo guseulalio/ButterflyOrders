334dce8690a0e291a021f17635c5ffe3768f5a8c
Gus Eulalio <guga.emc@gmail.com>
2021-10-09 15:03:05 +1100

    Initial Commit

---
+++ ButterflyOrders/ButterflyOrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButterflyOrdersApp.swift
+//  ButterflyOrders
+//
+//  Created by Gustavo E M Cabral on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ButterflyOrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
